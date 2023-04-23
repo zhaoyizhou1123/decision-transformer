63bc778769113d5d6a84b60730e6d054316962b4
Zhaoyi Zhou <zhouzhao20@mails.tsinghua.edu.cn>
2023-04-23 13:18:26 -0700

    Modified pkg versions for cuda

--- atari/run.sh
+++ atari/run.sh
@@ -41,11 +41,13 @@ do
 done
 
 # Own cmds
-python run_dt_atari.py --epochs 5 --model_type 'reward_conditioned' --num_steps 5000 --num_buffers 50 --game 'Breakout' --data_dir_prefix './at_replay'
-python run_dt_atari.py --epochs 1 --num_steps 100 --num_buffers 1
+python run_dt_atari.py --context_length 30 --epochs 5 --model_type 'reward_conditioned' --num_steps 500000 --num_buffers 50 --game 'Breakout' --batch_size 128
+python run_dt_atari.py --epochs 1 --num_steps 10 --num_buffers 1 --context_length 1 --batch_size 4
 # hyak cmds
-salloc -p gpu-a40 -A simondu --time=1:00:00 --nodes=1 --cpus-per-task=1 --mem=16G --gres=gpu:1
-salloc -p gpu-rtx6k -A cse --time=0:5:00 --nodes=1 --cpus-per-task=1 --mem=16G
+salloc -p gpu-a40 -A simondu --time=1:00:00 --nodes=1 --cpus-per-task=4 --mem=16G --gres=gpu:1
+salloc -p gpu-rtx6k -A cse --time=1:00:00 --nodes=1 --cpus-per-task=4 --mem=16G
 salloc -p gpu-2080ti -A cse --time=0:2:00 --nodes=1 --cpus-per-task=1 --mem=16G
 
-pip install atari-py pyprind absl-py gin-config gym tqdm blosc git+https://github.com/google/dopamine.git
+pip install atari-py==0.2.* pyprind==2.11.* jaxlib==0.1.* jax==0.1.* absl-py gin-config gym tqdm blosc tensorflow==2.2.* numpy==1.18.* opencv-python==3.4.* git+https://github.com/google/dopamine.git@366d6d0199def6328dbee2c1da4494e04a30fc78
+
+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 #github credential
