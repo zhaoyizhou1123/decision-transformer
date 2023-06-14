de61ff2f3b747d75386a26e973b2270652614a84
Zhaoyi Zhou <zhouzhao20@mails.tsinghua.edu.cn>
2023-06-14 01:56:24 -0700

    sbatch results

--- toy/cql.sh
+++ toy/cql.sh
@@ -1,20 +1,56 @@
 # python run_cql.py --epochs 100 --batch_size 4 --horizon 20 --data_file ./dataset/toy5_rev.csv --rate 6e-4 --arch '2' --env_path ./env/env_rev.txt --n_embd ${n_embd} --tradeoff_coef ${alpha} --tb_suffix $suffix
 # python run_cql.py --epochs 5 --batch_size 4 --horizon 20 --data_file ./dataset/toy5.csv --rate 6e-4 --arch '10-10' --env_path ./env/env_bandit.txt
 
-suffix=05251138
+suffix=06121834
+env=./env/env_bandit.txt
+data=./dataset/bandit_exp_hasha.csv
+# batch=8000
+lr=6e-4
+scale=1
+
 
 # for n_embd in 4 
 # do
 #     for alpha in 0
 #     do
-#         python run_cql.py --epochs 100 --batch_size 4 --horizon 20 --data_file ./dataset/toy5_rev.csv --rate 0.6 --arch '' --env_path ./env/env_rev.txt --n_embd ${n_embd} --tradeoff_coef ${alpha} --tb_suffix $suffix
+#         python run_cql.py --epochs 500 --batch_size 2000 --data_file ./dataset/rev_exp.csv --rate 0.05 --arch '' --env_path ./env/env_rev.txt --n_embd ${n_embd} --tradeoff_coef ${alpha} --tb_suffix $suffix
+#         python run_cql.py --epochs 500 --batch_size 2000 --data_file ./dataset/bandit_exp.csv --rate 0.05 --arch '' --env_path ./env/env_bandit.txt --n_embd ${n_embd} --tradeoff_coef ${alpha} --tb_suffix $suffix
+#         python run_cql.py --epochs 500 --batch_size 2000 --data_file ./dataset/toy6.csv --rate 0.05 --arch '' --env_path ./env/env_bandit.txt --n_embd ${n_embd} --tradeoff_coef ${alpha} --tb_suffix $suffix
 #     done
 # done
 
-for lr in 1.5 
+
+
+# for arch in '' '10' '10-10'
+# do
+#     for action_repeat in 10 1
+#     do 
+#         for scale in 1 10
+#         do
+#         # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '2' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+#         # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '5' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+#         # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '2-2' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+#         # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '5-5' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+#             python run_cql.py --epochs 500 --batch_size $batch --data_file ./dataset/3s-h20_exp.csv --rate $lr --arch $arch --env_path ./env/env_3s_h20.txt --tradeoff_coef 0 --tb_suffix $suffix --action_repeat ${action_repeat} --scale $scale
+#         done
+#     done
+# done
+
+for action_repeat in 1 10
 do
-    for batch in 200
+    for batch in 8000
     do
-        python run_cql.py --epochs 50000 --batch_size $batch --horizon 20 --data_file ./dataset/toy5_rev.csv --rate $lr --arch '' --env_path ./env/env_rev.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+        for n_embd in 1 2 4
+        do
+        # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '2' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+        # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '5' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+        # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '2-2' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+        # python run_cql.py --epochs 100 --batch_size $batch --data_file ./dataset/3s_rand.csv --rate $lr --arch '5-5' --env_path ./env/env_3s_rand.txt --n_embd 4 --tradeoff_coef 0 --tb_suffix $suffix
+            python run_cql.py --epochs 200 --batch_size $batch --data_file $data --rate $lr --arch '' --env_path $env --tradeoff_coef 0 --tb_suffix $suffix --repeat ${action_repeat} --scale $scale --n_embd ${n_embd}
+            # for arch in '1' '2' '5'
+            # do
+            #     python run_cql.py --epochs 200 --batch_size $batch --data_file $data --rate $lr --arch $arch --env_path $env --tradeoff_coef 0 --tb_suffix $suffix --repeat ${action_repeat} --scale $scale --n_embd ${n_embd}
+            # done
+        done
     done
 done
--- toy/run.sh
+++ toy/run.sh
@@ -3,14 +3,49 @@
 # source /opt/conda/etc/profile.d/conda.sh
 # conda activate dta
 
-# for ctx in 1 2 3
+# mm-dd-hour-min
+suffix=06121834
+env=./env/env_bandit.txt
+data=./dataset/bandit_exp_hasha.csv
+goal=20
+repeat=1
+batch=4
+lr=6e-4
+
+# for ctx in 1 2 4 6 8
 # do
-#     for goal in 4 5 10
-#     do
-#         echo python run_toy.py --context_length $ctx --epochs 3 --model_type 'reward_conditioned' --batch_size 1 --horizon 5 --goal $goal
-#         python run_toy.py --context_length $ctx --epochs 3 --model_type 'reward_conditioned' --batch_size 1 --horizon 5 --goal $goal
-#     done
+# python run_toy.py --context_length 1 --epochs 50 --env_path ./env/env_3s_rand.txt --batch_size 4 --goal 200 --data_file ./dataset/rand_exp.csv --rate 6e-4 --tb_suffix $suffix --model 'mlp'
+# python run_toy.py --context_length 2 --epochs 50 --env_path ./env/env_3s_rand.txt --batch_size 4 --goal 200 --data_file ./dataset/rand_exp.csv --rate 6e-4 --tb_suffix $suffix --model 'mlp'
 # done
+# python test.py --context_length 6 --epochs 200 --model_type 'reward_conditioned' --batch_size 4 --horizon 20 --goal 20 --data_file ./dataset/toy_g10.csv --rate 6e-4 --tb_suffix $suffix --ckpt_prefix ./model/m_rand_exp
+
+# dt
+# for ctx in 1 2 10 20
+# do
+#     python run_toy.py --context_length $ctx --epochs 200 --env_path ./env/env_mid-s.txt --batch_size 20 --goal 71 --data_file ./dataset/mid-s_exp.csv --rate 6e-4 --tb_suffix $suffix --model 'dt'
+# done
+
+# mlp
+
+for ctx in 1 2
+do
+    for n_embd in 1 2 4
+    # echo python run_toy.py --context_length $ctx --epochs 3 --model_type 'reward_conditioned' --batch_size 1 --horizon 5 --goal $goal --data_file ./dataset/toy5.csv --rate 6e-4
+    # python run_toy.py --context_length $ctx --epochs 100 --model_type 'reward_conditioned' --batch_size $batch --horizon 20 --goal $goal --data_file ./dataset/toy5.csv --rate 6e-4 --tb_suffix $suffix
+    # python run_toy.py --context_length $ctx --epochs 100 --model_type 'reward_conditioned' --batch_size $batch --horizon 20 --goal $goal --data_file ./dataset/toy_alt.csv --rate 6e-4 --tb_suffix $suffix
+
+    # python run_toy.py --context_length $ctx --epochs 500 --env_path ./env/env_alt-s.txt --batch_size 4 --goal 20 --data_file ./dataset/alt-s_exp.csv --rate 6e-4 --tb_suffix $suffix --model 'mlp'
+    # python run_toy.py --context_length $ctx --epochs 500 --env_path ./env/env_alt-s.txt --batch_size 4 --goal 20 --data_file ./dataset/alt-s_exp.csv --rate 6e-4 --tb_suffix $suffix --model 'mlp' --arch '10'
+    # python run_toy.py --context_length $ctx --epochs 500 --env_path ./env/env_alt-s.txt --batch_size 4 --goal 20 --data_file ./dataset/alt-s_exp.csv --rate 6e-4 --tb_suffix $suffix --model 'mlp' --arch '10-5-2'
+    do
+        python run_toy.py --context_length $ctx --epochs 200 --env_path $env --batch_size $batch --goal $goal --data_file $data --rate 6e-4 --tb_suffix $suffix --model 'mlp' --arch '' --repeat $repeat --n_embd ${n_embd} --rate $lr
+        # for arch in '1' '2' '5'
+        # do
+        #     python run_toy.py --context_length $ctx --epochs 200 --env_path $env --batch_size $batch --goal $goal --data_file $data --rate 6e-4 --tb_suffix $suffix --model 'mlp' --arch $arch --repeat $repeat --n_embd ${n_embd} --rate $lr
+        # done
+    done
+    # python run_toy.py --context_length $ctx --epochs 100 --model_type 'reward_conditioned' --batch_size $batch --horizon 20 --goal $goal --data_file ./dataset/toy_mix10.csv --rate 6e-4 --tb_suffix $suffix
+done
 
 # for batch in 1 2 3
 # do
@@ -25,8 +60,8 @@
 # python run_toy.py --context_length 20 --epochs 100 --model_type reward_conditioned --batch_size 4 --horizon 20 --goal 0 --data_file ./dataset/toy5.csv  --rate 6e-4 --ckpt_prefix ./model/m5_ctx20_g0
 
 # echo python run_toy.py --context_length 20 --epochs 100 --model_type reward_conditioned --batch_size 1 --horizon 20 --goal 20 --data_file ./dataset/toy5_rev.csv --rate 6e-4
-python run_toy.py --context_length 2 --epochs 75 --model_type reward_conditioned --batch_size 4 --horizon 20 --goal 20 --data_file ./dataset/toy_mix.csv --rate 6e-4
-python run_toy.py --context_length 2 --epochs 75 --model_type reward_conditioned --batch_size 4 --horizon 20 --goal 20 --data_file ./dataset/toy_mix.csv --rate 6e-4 --hash True
+# python run_toy.py --context_length 4 --epochs 75 --model_type reward_conditioned --batch_size 2 --horizon 20 --goal 20 --data_file ./dataset/toy5.csv --rate 6e-4
+# python run_toy.py --context_length 2 --epochs 75 --model_type reward_conditioned --batch_size 4 --horizon 20 --goal 20 --data_file ./dataset/toy_mix.csv --rate 6e-4 --hash True
 # python run_toy.py --context_length 20 --epochs 50 --model_type reward_conditioned --batch_size 4 --horizon 20 --goal 20 --data_file ./dataset/toy_mix.csv --rate 6e-4 --ckpt_prefix ./model/m_mix
 # python run_toy.py --context_length 2 --epochs 50 --model_type reward_conditioned --batch_size 4 --horizon 20 --goal 20 --data_file ./dataset/toy5.csv --rate 6e-4 
 
